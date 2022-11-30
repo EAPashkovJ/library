59ab709fc728a2e9e821ebe04f038f006e870990
mikhail-marisov <89081724+mikhail-marisov@users.noreply.github.com>
2022-11-30 09:41:42 +0300

    Delete bin directory

--- bin/src/main/resources/db/changelog/02_data_initialization.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO access_type (id, access)
-VALUES (1, 'admin');
-
-INSERT INTO access_type (id, access)
-VALUES (2, 'user');
-
-INSERT INTO users (id, name, points, access_type_id, email, password)
-VALUES (1, 'admin', 0, 1, 'mail@me.com', '0');
