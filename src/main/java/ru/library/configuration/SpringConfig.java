97fbbb9c72ca9083f59122f8afdf7a2b4030031e
Evgenii <eapashkov@gmail.com>
2022-12-01 18:34:38 +0300

    Изменил таблицу users на usrs postgr волнуется Добавил реализацию ролей в коллекции Set

--- src/main/java/ru/library/configuration/SpringConfig.java
+++ src/main/java/ru/library/configuration/SpringConfig.java
@@ -3,12 +3,16 @@ package ru.library.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import ru.library.service.UserDetailsServiceImpl;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
@@ -22,6 +26,22 @@ public class SpringConfig {
     @Bean
     public PasswordEncoder PasswordEncoder() {
         return new BCryptPasswordEncoder(12);
+    }
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+
+    private  PasswordEncoder passwordEncoder;
+
+    @Autowired
+
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser("user").password(passwordEncoder.encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder.encode("password")).roles("USER", "ADMIN");
     }
 //@Bean
 //public PasswordEncoder passwordEncoder() {
--- src/main/java/ru/library/configuration/WebSecurityConfiguration.java
+++ src/main/java/ru/library/configuration/WebSecurityConfiguration.java
@@ -1,13 +1,18 @@
 package ru.library.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+import java.security.cert.Extension;
 
 @EnableWebSecurity
 @Configuration
@@ -16,24 +21,24 @@ public class WebSecurityConfiguration extends
 
     public final PasswordEncoder passwordEncoder;
 
+
+
+
+
     public WebSecurityConfiguration(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
+        http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login").permitAll()
                 .antMatchers("/admin").hasRole("ADMIN")
-               .antMatchers("/user/").hasAnyRole("USER", "ADMIN")
-                .anyRequest().authenticated()
+                .antMatchers("login").permitAll()
                 .and()
-                .httpBasic()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
+                .authorizeRequests()
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("login").permitAll()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
