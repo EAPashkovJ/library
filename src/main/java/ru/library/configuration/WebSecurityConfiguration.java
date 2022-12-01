30fae88c41eadfaefc7c266b8a4f833dbb6241e0
Evgenii <eapashkov@gmail.com>
2022-12-01 19:23:11 +0300

    Изменил таблицу users на usrs postgr волнуется Добавил реализацию ролей в коллекции Set

--- src/main/java/ru/library/configuration/WebSecurityConfiguration.java
+++ src/main/java/ru/library/configuration/WebSecurityConfiguration.java
@@ -1,8 +1,8 @@
 package ru.library.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,19 +12,36 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
-import java.security.cert.Extension;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration extends
         WebSecurityConfigurerAdapter {
 
-    public final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+    private final DataSource dataSource;
 
-
-    public WebSecurityConfiguration(PasswordEncoder passwordEncoder) {
+    public WebSecurityConfiguration(@Lazy PasswordEncoder passwordEncoder, DataSource dataSource) {
         this.passwordEncoder = passwordEncoder;
+        this.dataSource = dataSource;
     }
+    @Bean
+    public PasswordEncoder PasswordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+
+
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser("user").password(passwordEncoder.encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder.encode("password")).roles("USER", "ADMIN");
+    }
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
